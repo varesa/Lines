a98d3593ee9fcdb14f86cec412d8df6546d59b62
Esa Varemo <esa@kuivanto.fi>
2012-05-13 18:17:58 +0300

    Added a base for the renderer

---
+++ src/fi/dy/esav/lines/LinesRenderer.java
@@ -0,0 +1,16 @@
+package fi.dy.esav.lines;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class LinesRenderer extends SurfaceView {
+
+	/**
+	 * @param context
+	 */
+	public LinesRenderer(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
