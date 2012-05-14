452e9d6a0543dd03c721f6a506ca9431d0735a81
Esa Varemo <esa@kuivanto.fi>
2012-05-14 09:05:30 +0300

    Fullscreen and no title

--- src/fi/dy/esav/lines/LinesActivity.java
+++ src/fi/dy/esav/lines/LinesActivity.java
@@ -2,6 +2,8 @@ package fi.dy.esav.lines;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LinesActivity extends Activity {
 	
@@ -10,6 +12,10 @@ public class LinesActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        
         renderer = new LinesRenderer(this);
         setContentView(renderer);
     }
