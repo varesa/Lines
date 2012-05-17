03387e55a9e9cf8c4645b9cd985f69affdcafb71
Esa Varemo <esa@kuivanto.fi>
2012-05-18 00:43:13 +0300

    Implemented a "legend" - draw a continuous line from the end of the furthest line

--- src/fi/dy/esav/lines/LinesRenderer.java
+++ src/fi/dy/esav/lines/LinesRenderer.java
@@ -1,10 +1,12 @@
 package fi.dy.esav.lines;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.util.Config;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -37,7 +39,9 @@ public class LinesRenderer extends SurfaceView implements Runnable {
 		}
 	}
 	
-	public void run() {		
+	public void run() {
+		if(getWidth() == 0 || getHeight() == 0) return;
+		
 		long startTime;
 
 		PointF center = new PointF(this.getWidth()/2, this.getHeight()/2);
@@ -50,6 +54,9 @@ public class LinesRenderer extends SurfaceView implements Runnable {
 		
 		Paint paint = new Paint();
 		paint.setColor(Color.BLUE);
+		
+		Bitmap legend = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+		legend.eraseColor(Color.WHITE);
 
 		while(running) {
 			startTime = System.currentTimeMillis();
@@ -57,9 +64,12 @@ public class LinesRenderer extends SurfaceView implements Runnable {
 			if(!holder.getSurface().isValid()) continue;
 			Canvas canvas = holder.lockCanvas();
 			
-			canvas.drawRGB(255, 255, 255);
+			//canvas.drawRGB(255, 255, 255);
+			canvas.drawBitmap(legend, 0, 0, null);
 			canvas.drawLines(line1.getPoints(), paint);
 			canvas.drawLines(line2.getPoints(), paint);
+			
+			legend.setPixel((int)line2.end.x, (int)line2.end.y, Color.BLACK);
 			
 			line1.incrementAngle();
 			line2.incrementAngle();
