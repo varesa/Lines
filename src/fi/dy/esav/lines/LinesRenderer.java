7ff53eabf44448fcd25587d1ee6d6d4eab0219e8
Esa Varemo <esa@kuivanto.fi>
2012-05-17 19:19:39 +0300

    Draw a line instead of solid color

--- src/fi/dy/esav/lines/LinesRenderer.java
+++ src/fi/dy/esav/lines/LinesRenderer.java
@@ -2,6 +2,9 @@ package fi.dy.esav.lines;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -41,22 +44,34 @@ public class LinesRenderer extends SurfaceView implements Runnable {
 		long endTime;
 		long dt;
 		
-		float r = 0;
+		Point center = new Point(this.getWidth()/2, this.getHeight()/2);
 		
+		double halfscreen_len = Math.min(this.getWidth(), this.getHeight())/2;
+		
+		double line1_len = halfscreen_len / 3 * 2;
+		double line1_ang = 15;
+		double line1_inc = 0.1;
+		float line1_pts[] = {center.x, center.y, (float) (center.x + line1_len * Math.cos(line1_ang)), (float) (center.y + line1_len * Math.sin(line1_ang))};
+		
+		Paint paint = new Paint();
+		paint.setColor(Color.BLUE);
+
 		while(running) {
 			if(!holder.getSurface().isValid()) {
 				continue;
 			}
 			
 			Canvas canvas = holder.lockCanvas();
-			// Do the drawing
-			canvas.drawRGB( (int)r, 0, 0);
-			r += 0.05;
 			
+			canvas.drawLines(line1_pts, paint);
 			
 			endTime = System.currentTimeMillis();
 		    dt = endTime - startTime;
-		    Log.d("Lines", String.valueOf(dt));
+		    
+		    line1_ang += line1_inc;
+		    line1_pts[2] = (float) (center.x + line1_len * Math.cos(line1_ang));
+		    line1_pts[3] = (float) (center.y + line1_len * Math.sin(line1_ang));
+		    
 		    if (dt < 20) {
 				try {
 					Thread.sleep(20 - dt);
